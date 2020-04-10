bb2d38d6a18a95728c1fff329525b43f224e8428
suziwen <suziwen1@gmail.com>
2020-04-10 09:10:24 +0800

    add link

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,33 @@
 import React from "react"
+import { MDXProvider } from '@mdx-js/react'
+import { Link } from "gatsby"
+
+import Content from "./content.mdx"
+
+
+const StyleLink = ({href, children, ...props})=>{
+  const to = props.to || href
+  return (
+    <Link
+      {...props}
+      to={to}
+      activeClassName="active"
+    >
+      {children}
+    </Link>
+  )
+}
+
+const components = {
+  a: StyleLink
+}
 
 export default ()=> {
   return (
-    <div>hhhh</div>
+    <div>
+      <MDXProvider components={components}>
+        <Content />
+      </MDXProvider>
+    </div>
   )
 }
---
+++ src/pages/link.js
@@ -0,0 +1,4 @@
+import React from "react"
+import { Link } from "gatsby"
+
+export default ()=> (<div>hello world <Link to={"/"}>go back</Link></div>)
